f09a4afc72c36376c2c075243c031679909c03dc
khanmarketk804-stack <khanmarketk804@gmail.com>
2026-08-15 11:39:53 +0500

    Update digikhata.ts

--- src/app/digikhata.ts
+++ src/app/digikhata.ts
@@ -2,8 +2,8 @@ import { Component, inject, signal, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
-import { StoreService } from '../services/store.service';
-import { Customer, Transaction } from '../models/store.models';
+import { StoreService } from './store.service';
+import { Customer, Transaction } from './store.models';
 
 @Component({
   selector: 'app-digikhata',
